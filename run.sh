e7ed842df60b7b14d5f5c65bb11471e34e54252a
Ingo Oppermann <ingo@oppermann.ch>
2018-10-09 17:16:39 +0200

    Fix wrong syntax in comparison of strings

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 CPU_TYPE=$(uname -m | cut -c 1-3)
-if [ "${MODE}" == "RASPICAM" ] && [ "$CPU_TYPE" == "arm" ];
-then
+if [ "${MODE}" = "RASPICAM" ] && [ "$CPU_TYPE" = "arm" ]; then
     echo "/opt/vc/lib" > /etc/ld.so.conf.d/00-vmcs.conf
     ldconfig
     npm start &
@@ -16,37 +15,32 @@ then
         fi
     done
 
-    if [ "$RS_RASPICAM_WIDTH" = "" ]
-    then
+    if [ "$RS_RASPICAM_WIDTH" = "" ]; then
         RASPICAM_WIDTH=1280
     else
         RASPICAM_WIDTH=$RS_RASPICAM_WIDTH
     fi
 
-    if [ "$RS_RASPICAM_HEIGHT" = "" ]
-    then
+    if [ "$RS_RASPICAM_HEIGHT" = "" ]; then
         RASPICAM_HEIGHT=720
     else
         RASPICAM_HEIGHT=$RS_RASPICAM_HEIGHT
     fi
 
-    if [ "$RS_RASPICAM_FPS" = "" ]
-    then
+    if [ "$RS_RASPICAM_FPS" = "" ]; then
         RASPICAM_FPS=25
     else
         RASPICAM_FPS=$RS_RASPICAM_FPS
     fi
 
-    if [ "$RS_RASPICAM_BITRATE" = "" ]
-    then
+    if [ "$RS_RASPICAM_BITRATE" = "" ]; then
         RASPICAM_BITRATE=50000
     else
         RASPICAM_BITRATE=$RS_RASPICAM_BITRATE
     fi
 
     /opt/vc/bin/raspivid -t 0 -w $RASPICAM_WIDTH -h $RASPICAM_HEIGHT -fps $RASPICAM_FPS -b $RASPICAM_BITRATE -o - | ffmpeg -i - -f lavfi -i aevalsrc=0 -vcodec copy -acodec aac -strict experimental -map 0:0 -map 1:0 -shortest -flags +global_header -f flv rtmp://127.0.0.1:1935/live/raspicam.stream > /dev/null 2>&1
-elif [ "${MODE}" == "USBCAM" ];
-then
+elif [ "${MODE}" = "USBCAM" ]; then
     apt-get update && apt-get install -y v4l-utils libv4l-0
     npm start &
     NGINX_RUNNING=0
