894b3611193d60700443dcbcf3dc869085e4a62e
Ingo Oppermann <ingo@oppermann.ch>
2018-11-05 16:09:37 +0100

    Add more RASPICAM parameter
    Add more parameter to control the raspicam. The raspivid allows
    many options to be tweaked in order to get the best result
    from the camera. Most of them are now changeable through many
    RS_RASPICAM_* environment variables. The defaults mostly
    corresponds to the ones from raspivid itself.

--- run.sh
+++ run.sh
@@ -15,31 +15,115 @@ if [ "${MODE}" = "RASPICAM" ] && [ "$CPU_TYPE" = "arm" ]; then
         fi
     done
 
-    if [ "$RS_RASPICAM_WIDTH" = "" ]; then
-        RASPICAM_WIDTH=1280
-    else
-        RASPICAM_WIDTH=$RS_RASPICAM_WIDTH
+    ## options
+
+    RASPIVID_OPTIONS="--timeout 0 --nopreview"
+
+    # flip the image horizontally
+    if [ "$RS_RASPICAM_HFLIP" = "true" ]; then
+        RASPIVID_OPTIONS="$RASPIVID_OPTIONS --hflip"
     fi
 
-    if [ "$RS_RASPICAM_HEIGHT" = "" ]; then
-        RASPICAM_HEIGHT=720
-    else
-        RASPICAM_HEIGHT=$RS_RASPICAM_HEIGHT
+    # flip the image vertically
+    if [ "$RS_RASPICAM_VFLIP" = "true" ]; then
+        RASPIVID_OPTIONS="$RASPIVID_OPTIONS --vflip"
     fi
 
-    if [ "$RS_RASPICAM_FPS" = "" ]; then
-        RASPICAM_FPS=25
-    else
+    # inline headers (SPS, PPS) -- https://en.wikipedia.org/wiki/X264
+    if [ "$RS_RASPICAM_INLINE" = "true" ]; then
+        RASPIVID_OPTIONS="$RASPIVID_OPTIONS --inline"
+    fi
+
+    # video stabilization
+    if [ "$RS_RASPICAM_STABILIZATION" = "true" ]; then
+        RASPIVID_OPTIONS="$RASPIVID_OPTIONS --vstab"
+    fi
+
+    ## FPS and GOP
+
+    RASPICAM_FPS=25
+    RASPICAM_GOP=50
+
+    if [ -n "$RS_RASPICAM_FPS" ]; then
         RASPICAM_FPS=$RS_RASPICAM_FPS
+        RASPICAM_GOP=$((RASPICAM_FPS * 2))
     fi
 
-    if [ "$RS_RASPICAM_BITRATE" = "" ]; then
-        RASPICAM_BITRATE=50000
-    else
-        RASPICAM_BITRATE=$RS_RASPICAM_BITRATE
+    if [ -n "$RS_RASPICAM_GOP" ]; then
+        RASPICAM_GOP=$RASPICAM_GOP
     fi
 
-    /opt/vc/bin/raspivid -t 0 -w $RASPICAM_WIDTH -h $RASPICAM_HEIGHT -fps $RASPICAM_FPS -b $RASPICAM_BITRATE -o - | ffmpeg -i - -f lavfi -i anullsrc=r=44100:cl=mono -vcodec copy -acodec aac -b:a 0k -map 0:v -map 1:a -shortest -f flv rtmp://127.0.0.1:1935/live/raspicam.stream > /dev/null 2>&1
+    ## bitrate and codec
+
+    RASPICAM_BITRATE=${RS_RASPICAM_BITRATE:=5242880}
+    # h264 profile: baseline (no B-frames), main, high
+    RASPICAM_H264PROFILE=${RS_RASPICAM_H264PROFILE:="high"}
+    # h264 level: 4, 4.1, 4.2 (1080p30, 720p60 and 640 × 480p60/90)
+    RASPICAM_H264LEVEL=${RS_RASPICAM_H264PROFILE:=4}
+    RASPICAM_CODEC=${RS_RASPICAM_CODEC:="H264"}
+
+    ## image parameter
+
+    RASPICAM_WIDTH=${RS_RASPICAM_WIDTH:=1920}
+    RASPICAM_HEIGHT=${RS_RASPICAM_HEIGHT:=1080}
+    # -100 to 100
+    RASPICAM_SHARPNESS=${RS_RASPICAM_SHARPNESS:=0}
+    # -100 to 100
+    RASPICAM_CONTRAST=${RS_RASPICAM_CONTRAST:=0}
+    # 0 to 100
+    RASPICAM_BRIGHTNESS=${RS_RASPICAM_BRIGHTNESS:=50}
+    # -100 to 100
+    RASPICAM_SATURATION=${RS_RASPICAM_SATURATION:=0}
+
+    ## image quality and effects
+
+    # capture ISO
+    RASPICAM_ISO=${RS_RASPICAM_ISO:=0}
+    # EV compensation, steps of 1/6 stop
+    RASPICAM_EV=${RS_RASPICAM_EV:=0}
+    # exposure: off,auto,night,nightpreview,backlight,spotlight,sports,snow,beach,verylong,fixedfps,antishake,fireworks
+    RASPICAM_EXPOSURE=${RS_RASPICAM_EXPOSURE:="auto"}
+    # flicker filter: off,auto,50hz,60hz
+    RASPICAM_FLICKER=${RS_RASPICAM_FLICKER:="off"}
+    # white balance: off,auto,sun,cloud,shade,tungsten,fluorescent,incandescent,flash,horizon
+    RASPICAM_AWB=${RS_RASPICAM_AWB:="auto"}
+    # image effects: none,negative,solarise,sketch,denoise,emboss,oilpaint,hatch,gpen,pastel,watercolour,film,blur,saturation,colourswap,washedout,posterise,colourpoint,colourbalance,cartoon
+    RASPICAM_IMXFX=${RS_RASPICAM_IMXFX:="none"}
+    # metering: average,spot,backlit,matrix
+    RASPICAM_METERING=${RS_RASPICAM_METERING:="average"}
+    # dynamic range compression: off,low,med,high
+    RASPICAM_DRC=${RS_RASPICAM_DRC:="off"}
+
+    ## RTMP URL
+
+    RTMP_URL="rtmp://127.0.0.1:1935/live/raspicam.stream"
+
+    if [ -n "$RS_TOKEN" ]; then
+        RTMP_URL="${RTMP_URL}?token=${RS_TOKEN}"
+    fi
+
+    /opt/vc/bin/raspivid \
+        $RASPIVID_OPTIONS \
+        --width $RASPICAM_WIDTH \
+        --height $RASPICAM_HEIGHT \
+        --framerate $RASPICAM_FPS \
+        --bitrate $RASPICAM_BITRATE \
+        --intra $RASPICAM_GOP \
+        --codec $RASPICAM_CODEC \
+        --profile $RASPICAM_H264PROFILE \
+        --level $RASPICAM_H264LEVEL \
+        --sharpness $RASPICAM_SHARPNESS \
+        --contrast $RASPICAM_CONTRAST \
+        --brightness $RASPICAM_BRIGHTNESS \
+        --saturation $RASPICAM_SATURATION \
+        --ISO $RASPICAM_ISO \
+        --exposure $RASPICAM_EXPOSURE \
+        --flicker $RASPICAM_FLICKER \
+        --awb $RASPICAM_AWB \
+        --imxfx $RASPICAM_IMXFX \
+        --metering $RASPICAM_METERING \
+        --drc $RASPICAM_DRC \
+        -o - | ffmpeg -i - -f lavfi -i anullsrc=r=44100:cl=mono -vcodec copy -acodec aac -b:a 0k -map 0:v -map 1:a -shortest -f flv "${RTMP_URL}" > /dev/null 2>&1
 elif [ "${MODE}" = "USBCAM" ]; then
     apt-get update && apt-get install -y v4l-utils libv4l-0
     npm start &
@@ -53,7 +137,21 @@ elif [ "${MODE}" = "USBCAM" ]; then
             sleep 5
         fi
     done
-    ffmpeg -f v4l2 -r 25 -s 1280x720 -i /dev/video0 -f lavfi -i anullsrc=r=44100:cl=mono -vcodec copy -acodec aac -b:a 0k -map 0:v -map 1:a -shortest -f flv rtmp://127.0.0.1:1935/live/usb.stream > /dev/null 2>&1
+
+    # https://trac.ffmpeg.org/wiki/Capture/Webcam
+    # https://www.ffmpeg.org/ffmpeg-devices.html#video4linux2_002c-v4l2
+
+    USBCAM_WIDTH=${RS_USBCAM_WIDTH:=1280}
+    USBCAM_HEIGHT=${RS_USBCAM_HEIGHT:=720}
+    USBCAM_FPS=${RS_USBCAM_FPS:=25}
+
+    RTMP_URL="rtmp://127.0.0.1:1935/live/usb.stream"
+
+    if [ -n "$RS_TOKEN" ]; then
+        RTMP_URL="${RTMP_URL}?token=${RS_TOKEN}"
+    fi
+
+    ffmpeg -f v4l2 -framerate $USBCAM_FPS -video_size "${USBCAM_WIDTH}x${USBCAM_HEIGHT}" -i /dev/video0 -f lavfi -i anullsrc=r=44100:cl=mono -vcodec copy -acodec aac -b:a 0k -map 0:v -map 1:a -shortest -f flv "${RTMP_URL}" > /dev/null 2>&1
 else
     npm start
 fi
