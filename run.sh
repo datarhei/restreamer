fa5d30fb212184c13a5410f96cc6edc5a4097e34
Jan Stabenow <moin@jan-stabenow.de>
2022-11-09 12:51:57 +0100

    Add ffmigrate

--- run.sh
+++ run.sh
@@ -10,6 +10,15 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
+# Run the FFmpeg migration program. In case a FFmpeg 5 binary is present, it will create a
+# backup of the current DB and modify the FFmpeg parameter such that they are compatible
+# with FFmpeg 5.
+
+./bin/ffmigrate
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
 # Create a hint for the admin interface if there is no index.html
 
 if ! [ -f "${CORE_STORAGE_DISK_DIR}/index.html" ]; then
